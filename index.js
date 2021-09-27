6996fee946d27096191118308cf575477184cfcf
Ishaan Gaba <ishaangaba97@gmail.com>
2021-09-27 13:54:13 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function updateMap() {
+  fetch("/data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        cases = element.infected;
+        if(cases>255)
+        {
+            color = "rgb(255,0,0)";
+        }
+        else
+        {
+            color = `rgb(${cases},0,0)`;
+        }
+        // mark on the map
+        new mapboxgl.Marker({
+          draggable: false,
+          color: color
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    });
+}
+let interval = 2000;
+setInterval(updateMap(), interval);
